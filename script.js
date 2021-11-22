17c9d22e679d3dc3c3d165ec532f1b1013603e9a
D10S <dschi002@fiu.edu>
2021-11-22 10:53:36 -0500

    Adds Leaflet Map library and a couple ELs to mark the map.

--- script.js
+++ script.js
@@ -10,3 +10,103 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
+    map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Handling clicks on map
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    });
+  },
+  function () {
+    alert(
+      'Could not get your position. Please allow this site to access your location'
+    );
+  }
+);
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Display marker
+  L.marker(mapEvent.latlng)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+
+  // Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  form.classList.add('hidden');
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
+
+class Workout {
+  constructor(distance, duration, coords) {
+    this.distance = distance;
+    this.duration = duration;
+    this.coords = coords;
+  }
+}
+
+class Running extends Workout {
+  constructor(distance, duration, coords, cadence, pace) {
+    super(distance, duration, coords);
+    this.cadence = cadence;
+    this.pace = pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(distance, duration, coords, elevationGain, speed) {
+    super(distance, duration, coords);
+    this.elevationGain = elevationGain;
+    this.speed = speed;
+  }
+}
+
+class App {
+  constructor(workouts, map) {
+    this.workouts = workouts;
+    this.map = map;
+  }
+
+  _getPosition() {}
+
+  _loadMap(position) {}
+
+  _showForm() {}
+
+  _toggleElevationField() {}
+
+  _newWorkout() {}
+}
